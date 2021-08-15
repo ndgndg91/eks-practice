72c98a542f1e0372589abaeb741719ab30ef0277
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 <ndgndg91@gmail.com>
2021-08-15 04:18:04 +0000

    <feat> remove vpc modules

--- terraform/prod/vpc/main.tf
+++ terraform/prod/vpc/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    region         = "ap-northeast-2"
+    bucket         = "eks-terraform-workshop-ndgndg91"
+    key            = "vpc/terraform.tfstate"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 locals {
   application_vpc_instance_tenancy = "default"
   rds_vpc_instance_tenancy = "default"
