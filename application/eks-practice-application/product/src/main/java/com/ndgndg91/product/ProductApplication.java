d21c362a29a351a4ae5bc3a626ae50921c2d9b5d
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 <ndgndg91@gmail.com>
2021-08-29 23:16:33 +0900

    <feat> service discovery eureka

--- application/eks-practice-application/product/src/main/java/com/ndgndg91/product/ProductApplication.java
+++ application/eks-practice-application/product/src/main/java/com/ndgndg91/product/ProductApplication.java
@@ -2,7 +2,9 @@ package com.ndgndg91.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductApplication {
 
