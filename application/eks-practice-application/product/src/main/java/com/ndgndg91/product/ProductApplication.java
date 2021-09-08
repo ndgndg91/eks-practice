28ea97f8b022bf877377e49457ab730aec4bec0e
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 <ndgndg91@gmail.com>
2021-09-08 22:17:31 +0900

    <fix> order,product,seller-auth compile + batch Dockerfile

--- application/eks-practice-application/product/src/main/java/com/ndgndg91/product/ProductApplication.java
+++ application/eks-practice-application/product/src/main/java/com/ndgndg91/product/ProductApplication.java
@@ -2,9 +2,7 @@ package com.ndgndg91.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class ProductApplication {
 
