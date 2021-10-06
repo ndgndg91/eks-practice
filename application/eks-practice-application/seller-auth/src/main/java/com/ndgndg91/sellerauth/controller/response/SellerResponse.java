e2d894eeaadda788bb500d4b0d7dcbb9a462588b
ndgndg91 <ndgndg91@gmail.com>
2021-10-06 23:28:31 +0900

    <refactor> remove role on seller response, add marketing agreement

--- application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/controller/response/SellerResponse.java
+++ application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/controller/response/SellerResponse.java
@@ -9,12 +9,12 @@ import lombok.ToString;
 public final class SellerResponse {
     private final long id;
     private final String identifier;
-    private final String role;
+    private final boolean marketingAgreement;
 
     public SellerResponse(Seller seller) {
         this.id = seller.getId();
         this.identifier = seller.getIdentifier();
-        this.role = seller.getRole().name();
+        this.marketingAgreement = seller.getAgreement().isMarketingAgreement();
     }
 
 }
