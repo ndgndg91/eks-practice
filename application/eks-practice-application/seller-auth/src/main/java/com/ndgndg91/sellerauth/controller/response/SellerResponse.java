e784d675e250d9fa9bace8b641fba512023c70ae
ndgndg91 <ndgndg91@gmail.com>
2021-10-06 23:19:28 +0900

    <refactor> remove full name on seller response

--- application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/controller/response/SellerResponse.java
+++ application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/controller/response/SellerResponse.java
@@ -9,13 +9,11 @@ import lombok.ToString;
 public final class SellerResponse {
     private final long id;
     private final String identifier;
-    private final String fullName;
     private final String role;
 
     public SellerResponse(Seller seller) {
         this.id = seller.getId();
         this.identifier = seller.getIdentifier();
-        this.fullName = seller.getFullName();
         this.role = seller.getRole().name();
     }
 
