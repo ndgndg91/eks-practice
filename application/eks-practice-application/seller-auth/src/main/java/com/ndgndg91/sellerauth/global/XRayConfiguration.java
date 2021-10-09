2ccf23dad051dd6b00491c5c4923309fb6edf991
ndgndg91 <ndgndg91@gmail.com>
2021-10-09 23:43:05 +0900

    <feat> seller application xray eks plugin

--- application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/global/XRayConfiguration.java
+++ application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/global/XRayConfiguration.java
@@ -1,11 +1,26 @@
 package com.ndgndg91.sellerauth.global;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.EKSPlugin;
+import com.amazonaws.xray.strategy.DefaultStreamingStrategy;
+import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class XRayConfiguration {
+
+    @PostConstruct
+    public void init() {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new EKSPlugin("eks-workshop-cluster"));
+        builder.withStreamingStrategy(new DefaultStreamingStrategy(30));
+        AWSXRayRecorder globalRecorder = builder.build();
+        AWSXRay.setGlobalRecorder(globalRecorder);
+    }
 
     @Bean
     public AWSXRayServletFilter awsxRayServletFilter(){
