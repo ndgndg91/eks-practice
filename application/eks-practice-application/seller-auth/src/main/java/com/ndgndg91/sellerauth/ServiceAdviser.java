05fb11d5fbc61466c690fcae68354a74ecd98262
ndgndg91 <ndgndg91@gmail.com>
2021-10-09 22:55:53 +0900

    <feat> seller application rest controller adviser

---
+++ application/eks-practice-application/seller-auth/src/main/java/com/ndgndg91/sellerauth/ServiceAdviser.java
@@ -0,0 +1,16 @@
+package com.ndgndg91.sellerauth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ServiceAdviser {
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ApiError> handleServiceException(ServiceException e) {
+        return ResponseEntity
+            .status(e.getStatusCode())
+            .body(new ApiError(e.getErrorCode(), e.getErrorMessage()));
+    }
+}
