06a9a20f720874907805d6be56b0bb16000ab1d7
ndgndg91 <ndgndg91@gmail.com>
2021-10-10 12:45:52 +0000

    <feat> xray all application

--- terraform/prod/eks/yaml/xray/clean.sh
+++ terraform/prod/eks/yaml/xray/clean.sh
@@ -1,3 +1,2 @@
-kubectl delete label serviceaccount xray-daemon
 kubectl delete -f daemonset.yaml
 eksctl delete iamserviceaccount --name xray-daemon --namespace eks-workshop --cluster eks-workshop-cluster
--- terraform/prod/eks/yaml/xray/init.sh
+++ terraform/prod/eks/yaml/xray/init.sh
@@ -1,6 +1,5 @@
-eksctl utils associate-iam-oidc-provider --region=ap-northeast-2 --cluster=eks-workshop-cluster --approve
 eksctl create iamserviceaccount --name xray-daemon --namespace eks-workshop --cluster eks-workshop-cluster --attach-policy-arn arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess --approve --override-existing-serviceaccounts
-kubectl label serviceaccount xray-daemon app=xray-daemon
+kubectl label serviceaccount xray-daemon app=xray-daemon -n eks-workshop
 kubectl create -f daemonset.yaml
-kubectl describe daemonset xray-daemon
-kubectl logs -l app=xray-daemon
+kubectl describe daemonset xray-daemon -n eks-workshop
+kubectl logs -l app=xray-daemon -n eks-workshop
